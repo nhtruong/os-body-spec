5c12377156fdddc3023a112f75b69357a40ba2ec
Theo Truong <theotr@amazon.com>
2024-01-02 17:10:24 -0700

    # x-serialize

--- src/Merger.ts
+++ src/Merger.ts
@@ -5,11 +5,11 @@ import fs from "fs";
 export default class Merger {
     es: OpenAPIV3.Document;
     os: OpenAPIV3.Document;
-    os_desc: OpenAPIV3.Document;
-    constructor(es_spec: OpenAPIV3.Document, os_spec: OpenAPIV3.Document, os_desc: OpenAPIV3.Document) {
+    os_resolved: OpenAPIV3.Document;
+    constructor(es_spec: OpenAPIV3.Document, os_spec: OpenAPIV3.Document, os_resolved: OpenAPIV3.Document) {
         this.es = es_spec;
         this.os = os_spec;
-        this.os_desc = os_desc;
+        this.os_resolved = os_resolved;
     }
     
     static async init(es_path: string, os_path: string) {
@@ -38,7 +38,7 @@ export default class Merger {
     #merge_bodies(): void {
         const os = this.os.paths!;
         const es = this.es.paths!;
-        const desc = this.os_desc.paths!;
+        const resolved = this.os_resolved.paths!;
 
         for(const path in os) {
             if(es[path] === undefined) continue;
@@ -51,33 +51,32 @@ export default class Merger {
                 const os_op = os[path]![m]!;
                 const es_op = es[path]![m]!;
 
-                this.#transfer_requestBody(os_op, es_op, this.#get_description(desc[path]![m]!));
+                this.#transfer_requestBody(os_op, es_op, resolved[path]![m]!);
                 os_op.responses = es_op.responses
             }
         }
     }
 
-    #transfer_requestBody(os: OpenAPIV3.OperationObject, es: OpenAPIV3.OperationObject, desc: string | undefined): void {
+    #transfer_requestBody(os: OpenAPIV3.OperationObject, es: OpenAPIV3.OperationObject, resolved: OpenAPIV3.OperationObject): void {
         if(es.requestBody === undefined) return;
+        const resolved_body = resolved.requestBody as OpenAPIV3.RequestBodyObject;
+        const resolved_schema = resolved_body?.content!['application/json']!.schema as OpenAPIV3.SchemaObject;
+
         os.requestBody = es.requestBody;
-        if(desc === undefined) return;
+        if(resolved_schema === undefined) return;
 
-        const ref = (es.requestBody as OpenAPIV3.ReferenceObject).$ref;
-        const requestBody = (ref ? this.#traverse(this.es, ref) : es.requestBody) as OpenAPIV3.RequestBodyObject;
-        const schema = requestBody.content!['application/json']!.schema as OpenAPIV3.SchemaObject;
-        schema.description = desc;
+        const es_requestBody = this.#resolve(es.requestBody, this.es) as OpenAPIV3.RequestBodyObject;
+        const schema = es_requestBody.content['application/json']!.schema as OpenAPIV3.SchemaObject;
+        schema.description = resolved_schema?.description;
+        schema['x-serialize' as keyof OpenAPIV3.SchemaObject] = resolved_schema['x-serialize' as keyof OpenAPIV3.SchemaObject];
     }
 
-    #get_description(op: OpenAPIV3.OperationObject): string | undefined {
-        const requestBody = op.requestBody as OpenAPIV3.RequestBodyObject;
-        const schema = requestBody?.content!['application/json']!.schema as OpenAPIV3.SchemaObject;
-        return schema?.description;
-    }
+    #resolve(obj: Record<string, any> | undefined, root: Record<string, any>): Record<string, any> | undefined {
+        if(obj === undefined || obj.$ref === undefined) return obj;
 
-    #traverse(obj: Record<string, any>, path: string): Record<string, any> {
-        const paths = path.split('/');
+        const paths = obj.$ref.split('/');
         paths.shift();
-        for(const p of paths) { obj = obj[p]; }
-        return obj;
+        for(const p of paths) { root = root[p]; }
+        return root;
     }
 }
