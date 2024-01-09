0f286d380dba841d624506c96e43f8b59ef4ee93
Theo Truong <theotr@amazon.com>
2024-01-09 10:48:30 -0700

    # Rearranged Files

--- src/helpers.ts
+++ src/helpers.ts
@@ -6,3 +6,17 @@ export function resolve(obj: Record<string, any> | undefined, root: Record<strin
     for(const p of paths) { root = root[p]; }
     return root;
 }
+
+export const snake2Camel = (str: string, upper = true) => {
+    if (upper) return str.split('_').map((s) => s[0].toUpperCase() + s.slice(1)).join('');
+    return str.split('_').map((s, i) => i === 0 ? s : s[0].toUpperCase() + s.slice(1)).join('');
+}
+
+export const snake2Hyphen = (str: string, capitalized = true) => {
+    if (capitalized) return str.split('_').map((s) => s[0].toUpperCase() + s.slice(1)).join('-');
+    return str.replace(/_/g, '-');
+}
+
+export const snake2Capitalized = (str: string) => {
+    return str.split('_').map((s) => s[0].toUpperCase() + s.slice(1)).join(' ');
+}
